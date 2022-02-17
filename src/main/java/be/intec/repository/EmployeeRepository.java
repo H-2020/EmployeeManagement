e29dedb2273e9dc0e1c6cd97a8d77978459c74d4
Hilal Demir <hlldmr23@gmail.com>
2022-02-17 10:06:09 +0100

    First version

---
+++ src/main/java/be/intec/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package be.intec.repository;
+
+import be.intec.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
